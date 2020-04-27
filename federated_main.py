a37fa4639a4a401335b5cdccb42a53c3041c334b
Theo Hu <theohuzeou@gmail.com>
2020-04-27 13:48:25 -0400

    fixed minor issues

--- federated_main.py
+++ federated_main.py
@@ -99,6 +99,9 @@ if __name__ == '__main__':
     print_every = 10
     val_loss_pre, counter = 0, 0
 
+    eta = args.global_lr
+    decay = args.global_lr_decay
+
     for epoch in tqdm(range(args.epochs)):
         local_weights, local_losses = [], []
         print(f'\n | Global Training Round : {epoch+1} |\n')
@@ -142,7 +145,7 @@ if __name__ == '__main__':
         new_global_weights = copy.deepcopy(old_global_weights)
         for key in new_global_weights.keys():
             for i in range(n):
-                new_global_weights[key] += gradient_coefficients[i] * local_weights[i][key]
+                new_global_weights[key] += eta * gradient_coefficients[i] * local_weights[i][key]
 
 
 
@@ -215,34 +218,33 @@ if __name__ == '__main__':
     # file_name = "save/objects/N[{}]EPS[{}]{}_{}_{}_C[{}]_iid[{}]_E[{}]_B[{}].pickle".format(args.normalize,args.epsilon,args.dataset, args.model, args.epochs, args.frac, args.iid,
     #            args.local_ep, args.local_bs)
 
-    
-    file_name = "save/objects/Ep[{}]_frac[{}]_{}_{}_{}_Mom[{}]_{}_N[{}]_EPS[{}]_{}_iid[{}]_seed[{}].pickle".format(args.epochs,
-                                                                                            args.frac, args.local_ep,
-                                                                                            args.local_bs, args.lr,
-                                                                                            args.momentum, args.model,
-                                                                                            args.normalize,args.epsilon,
-                                                                                            args.dataset,args.iid,args.seed)
-
-    file_name2 = "save/objects/Ep[{}]_frac[{}]_{}_{}_{}_Mom[{}]_{}_N[{}]_EPS[{}]_{}_iid[{}]_seed[{}].log".format(
+    file_name = "Ep[{}]_frac[{}]_{}_{}_{}_{}_Mom[{}]_{}_N[{}]_EPS[{}]_{}_iid[{}]_seed[{}]_decay[{}].pickle".format(
         args.epochs,
         args.frac, args.local_ep,
-        args.local_bs, args.lr,
+        args.local_bs, args.lr, args.global_lr,
         args.momentum, args.model,
         args.normalize, args.epsilon,
-        args.dataset, args.iid, args.seed)
+        args.dataset, args.iid, args.seed, args.global_lr_decay)
+
+    file_name2 = "Ep[{}]_frac[{}]_{}_{}_{}_{}_Mom[{}]_{}_N[{}]_EPS[{}]_{}_iid[{}]_seed[{}]_decay[{}].log".format(
+        args.epochs,
+        args.frac, args.local_ep,
+        args.local_bs, args.lr, args.global_lr,
+        args.momentum, args.model,
+        args.normalize, args.epsilon,
+        args.dataset, args.iid, args.seed, args.global_lr_decay)
     
     folder="save/objects"
 
     THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-    my_file = os.path.join(THIS_FOLDER, file_name)
-    my_file2 = os.path.join(THIS_FOLDER, file_name2)
-
-
-    my_folder = os.path.join(THIS_FOLDER,folder)
+    my_folder = os.path.join(THIS_FOLDER, folder)
 
     # make sure the folder exists
     if not os.path.isdir(my_folder):
         os.mkdir(my_folder)
+
+    my_file = os.path.join(my_folder, file_name)
+    my_file2 = os.path.join(my_folder, file_name2)
 
 
     if args.inference:
@@ -261,6 +263,8 @@ if __name__ == '__main__':
         txtfile.write("\n Number of local epochs per round: {}".format(args.local_ep))
         txtfile.write("\n Local minibatch size: {}".format(args.local_bs))
         txtfile.write("\n Local learning rate: {}".format(args.lr))
+        txtfile.write("\n Global learning rate: {}".format(args.global_lr))
+        txtfile.write("\n Global learning rate decay: {}".format(args.global_lr_decay))
         txtfile.write("\n SGD momentum: {}".format(args.momentum))
         txtfile.write("\n NN model: {}".format(args.model))
         txtfile.write("\n Gradient Normalization: {}".format(args.normalize))
